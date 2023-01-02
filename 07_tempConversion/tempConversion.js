5973341b73b37dec4575e32b45baf262ac5ef3a7
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-02 17:09:16 -0600

    Add return to both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = function(fahrenheitTemp) {
   const celsiusTempConverted = Math.round(((fahrenheitTemp - 32) * (5 / 9)* 10)) / 10;
+  return celsiusTempConverted;
 };
 
 const convertToFahrenheit = function(celsiusTemp) {
   const fahrenheitTempConverted = Math.round((((celsiusTemp) * (9 / 5)) + 32)) / 10;
+  return fahrenheitTempConverted;
 };
 
 // Do not edit below this line
