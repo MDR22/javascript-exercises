97c6de15556f7f6d51d6796d2d477834e17b2be6
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-02 17:12:10 -0600

    Fix parantheses math round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(fahrenheitTemp) {
-  const celsiusTempConverted = Math.round(((fahrenheitTemp - 32) * (5 / 9)* 10)) / 10;
+  const celsiusTempConverted = Math.round(((fahrenheitTemp - 32) * (5 / 9)) * 10) / 10;
   return celsiusTempConverted;
 };
 
 const convertToFahrenheit = function(celsiusTemp) {
-  const fahrenheitTempConverted = Math.round((((celsiusTemp) * (9 / 5)) + 32)) / 10;
+  const fahrenheitTempConverted = Math.round(((celsiusTemp * (9 / 5)) + 32) * 10) / 10;
   return fahrenheitTempConverted;
 };
 
