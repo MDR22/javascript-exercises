d539bb52891a5af07663c68e9af2262683ce7299
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-03 19:30:19 -0600

    Add palindrome function and perform tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,9 @@
 const palindromes = function (string) {
     const stringLower = string.toLowerCase();
-    const stringReverse = stringLower.split('').reverse().join('');
-    return stringReverse;
-};
+    const stringRemoveNonAlphas = stringLower.replace(/[^a-z0-9]/gi, '');
+    const stringReverse = stringRemoveNonAlphas.split('').reverse().join('');
+    return stringReverse === stringRemoveNonAlphas;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
